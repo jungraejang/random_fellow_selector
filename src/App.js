e4c4695f01d69ebf05c93963438507772ffe230b
jungraejang <jungraejang@pursuit.org>
2019-07-14 16:14:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fellowNames: [
+        "Michael",
+        "Sihame",
+        "Amine",
+        "Voniel",
+        "Hupaul",
+        "Elisandra",
+        "Sergio",
+        "Shayna",
+        "Johanne",
+        "Jonathan",
+        "Peter",
+        "Dantae",
+        "Deja",
+        "Aransa",
+        "Suzette",
+        "Pearl",
+        "Camille",
+        "Owen",
+        "Sharrar",
+        "Douglas",
+        "Savita",
+        "Briahana",
+        "Kameron",
+        "Karen",
+        "Jenesh",
+        "Chukwuka",
+        "Joseph",
+        "Kathy",
+        "Ohidur",
+        "Stephanie",
+        "Ariel",
+        "Renee",
+        "Giselle",
+        "Briany",
+        "Malik",
+        "Kadijah"
+      ],
+      display: ""
+    };
+  }
+
+  buttonClick = (e) => {
+    e.preventDefault()
+    let randomIndex = Math.floor(Math.random() * this.state.fellowNames.length)
+    this.setState({
+      display: this.state.fellowNames[randomIndex]
+    })
+  }
+
+  render() {
+    return (
+      <div className="mainDiv">
+        <button className="randomButton" onClick={this.buttonClick}>Pick your "volunteer"</button>
+        <h1 className="nameText">{this.state.display}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
